63b7657368c4cb6a4cebd748ade2d42c0597503f
Malav Naagar <malavnagar90@gmail.com>
2021-10-09 11:54:09 +0530

    read excel file in class componments

--- src/App.js
+++ src/App.js
@@ -1,83 +1,12 @@
-// import logo from './logo.svg';
-import './App.css';
-import React,{useState} from 'react'
-import * as XLSX from 'xlsx'
-import BootstrapTable from 'react-bootstrap-table-next'
-import paginationFactory from 'react-bootstrap-table2-paginator'
-import cellEditFactory from 'react-bootstrap-table2-editor';
-const App =  () =>{
-  const [data,setData] = useState([])
-  const readExcel = (xlFile) =>{
-      const promise = new Promise((resolve,reject)=>{
-        const fileReader  = new FileReader()
-        fileReader.readAsArrayBuffer(xlFile);
-        
-        fileReader.onload = (e) =>{
-          const bufferArray = e.target.result;
-          const wb = XLSX.read(bufferArray,{type:"buffer"});
-          const wsname = wb.SheetNames[0]
-          // console.log(wsname)
-          const ws = wb.Sheets[wsname]
-          // console.log(ws)
-          const data = XLSX.utils.sheet_to_json(ws)
-          // console.log(data)
-          resolve(data)
-        }
-        fileReader.onerror = (err) =>{
-          reject(err)
-        }
-      })
-      promise.then((d)=>{
-        console.log(d)
-        setData(d)
-      }).catch((e)=>{
-        console.log(e.message)
-      })
-  }
-  const columns = [
-    {
-        dataField: "id",
-        text: "Id",
-        hidden: true,
-        csvExport: false,
-    },
-    {
-        dataField:"Contact Name",
-        text:"FieldName",
-        headerStyle: { minWidth: "200px" },
-    },
-    {
-        dataField:" phone no",
-        text:"Contact no"
-    },
-    // {
-    //   dataField:"Descriptions",
-    //   text:"Description"
-    // },
-    // {
-    //     dataField: "operation",
-    //     text: "Action",
-    //     // formatter:(id)=>formatterButton(id)
-    // },
-]
-  return(
-    <>
-    {/* {console.log(data)} */}
-      <input type='file' onChange={(e)=>{
-        const file = e.target.files[0]
-        readExcel(file)
-      }}/>
-      <BootstrapTable
-              keyField="id"
-              data={data}
-              columns={columns}
-              pagination={paginationFactory()}
-              noDataIndication="Table is Empty"
-              cellEdit={ cellEditFactory({ mode: 'dbclick',blurToSave: true }) }
-            />
-      
-    </>
+import React from 'react'
+import Dashboard2 from './exel'
+
+const App = ()=> {
+  return (
+    <div>
+      <Dashboard2/>
+    </div>
   )
 }
 
-export default App;
+export default App
---
+++ src/exel.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { ExcelRenderer, OutTable } from "react-excel-renderer";
+
+class Dashboard2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rows: [],
+      cols: []
+    };
+  }
+//  WORKING CODE ---------------------
+  changeHandler(event) {
+    let fileObj = event.target.files[0];
+    //just pass the fileObj as parameter
+    ExcelRenderer(fileObj, (err, resp) => {
+      if (err) {
+        console.log(err);
+      } else {
+        this.setState({
+          cols: resp.cols,
+          rows: resp.rows
+        });
+        console.log(this.state);
+      }
+    });
+  }
+  //  WORKING CODE ---------------------
+
+
+  render() {
+    return (
+      <div>
+        <h1>Upload File</h1>
+        <input
+          className="btn"
+          type="file"
+          onChange={this.changeHandler.bind(this)}
+          style={{ padding: "10px" }}
+        />
+        <OutTable
+          data={this.state.rows}
+          columns={this.state.cols}
+          tableClassName="ExcelTable2019"
+          tableHeaderRowClass="heading"
+        />
+      </div>
+    );
+  }
+}
+
+export default Dashboard2;
